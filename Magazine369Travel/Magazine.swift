ed63ec2a1caa067f06d70fdee06cc5f0d2d5466e
Reimos7 <tjrvud9@gmail.com>
2025-07-11 21:34:02 +0900

    feat: Magazine 더미데이터 및 구조체 추가

---
+++ Magazine369Travel/Magazine.swift
@@ -0,0 +1,17 @@
+//
+//  Magazine.swift
+//  Magazine369Travel
+//
+//  Created by Reimos on 7/11/25.
+//
+
+import Foundation
+
+struct Magazine {
+    let title: String
+    let subtitle: String
+    let photo_image: String
+    let date: String
+    let link: String
+    
+}
