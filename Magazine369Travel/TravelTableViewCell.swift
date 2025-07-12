976fe5d50978e4152ab53ae661eaf253205d3ad5
Reimos7 <tjrvud9@gmail.com>
2025-07-12 19:58:57 +0900

    feat: Travel 도시 셀 기본 오토레이아웃 설정

---
+++ Magazine369Travel/TravelTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TravelTableViewCell.swift
+//  Magazine369Travel
+//
+//  Created by Reimos on 7/12/25.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subTitleLabel: UILabel!
+    
+    @IBOutlet var starImageLabel: UILabel!
+    @IBOutlet var gradeSaveLabel: UILabel!
+    
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var travelImage: UIImageView!
+}
---
+++ Magazine369Travel/TravelTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TravelTableViewController.swift
+//  Magazine369Travel
+//
+//  Created by Reimos on 7/12/25.
+//
+
+import UIKit
+
+class TravelTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+   
+
+}
