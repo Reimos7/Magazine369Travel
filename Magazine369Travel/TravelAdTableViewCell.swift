587be8e6d6a7ceefb53b4fff401e5fd6fe40a4ba
Reimos7 <tjrvud9@gmail.com>
2025-07-13 13:03:48 +0900

    feat: Travel 광고 셀 오토레이아웃 구현

---
+++ Magazine369Travel/TravelAdTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAdTableViewCell.swift
+//  Magazine369Travel
+//
+//  Created by Reimos on 7/13/25.
+//
+
+import UIKit
+
+class TravelAdTableViewCell: UITableViewCell {
+
+    @IBOutlet var adTitleLabel: UILabel!
+    @IBOutlet var adBoxView: UIView!
+    @IBOutlet var adLabel: UILabel!
+    
+    
+}
