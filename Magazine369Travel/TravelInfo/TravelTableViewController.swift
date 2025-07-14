3251c90007f39f794899488e27b5d658e4b80d71
Reimos7 <tjrvud9@gmail.com>
2025-07-14 17:57:06 +0900

    feat: 광고 셀 클릭시 toastMessage 하단에 보여주는 기능 추가

--- Magazine369Travel/TravelInfo/TravelTableViewController.swift
+++ Magazine369Travel/TravelInfo/TravelTableViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Kingfisher
+import Toast
 
 class TravelTableViewController: UITableViewController {
     
@@ -59,6 +60,17 @@ class TravelTableViewController: UITableViewController {
         }
         // 일반 셀이면 200
         return 200
+    }
+    
+    // MARK: - 광고 셀 클릭시 toastMessage 하단에 보여주기
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let ad = travelInfo.travel[indexPath.row].ad, ad {
+            // toast 메시지
+            // duration 몇 초 동안 보여줄지
+            // position 위치, top이나 center는 키보드 때매 올라가니까 그럴때 사용하긴 함
+            // 뷰 기준으로 나오는거니까 다르게 사용하면 제대로 적용 안될 가능성이 큼
+            self.navigationController?.view.makeToast("광고 셀입니다", duration: 2.0, position: .bottom)
+        }
     }
     
     // 좋아요 버튼 상태 변경
