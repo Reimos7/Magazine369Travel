ee754841ae809edd1884ba246ecaae9912e7c5b3
Reimos7 <tjrvud9@gmail.com>
2025-07-16 02:20:27 +0900

    feat: textField를 통한 검색 기능 구현 엔터키 클릭 시, 실시간 검색 구현 모두, 국내, 해외 기준 검색 필터링 대소문자 구분없이 검색 적용

--- Magazine369Travel/City/CityTableViewController.swift
+++ Magazine369Travel/City/CityTableViewController.swift
@@ -12,6 +12,7 @@ class CityTableViewController: UITableViewController {
     
     let cityInfo = CityInfo()
     
+    @IBOutlet var textField: UITextField!
     @IBOutlet var segmentControl: UISegmentedControl!
     
     override func viewDidLoad() {
@@ -25,21 +26,38 @@ class CityTableViewController: UITableViewController {
     }
     
     
-    // MARK: - 0 , 1 , 2 조건에 따라 
+    // MARK: - 0 , 1 , 2 조건에 따라
     func filteredCity() -> [City] {
+        
+        let filteredCityList: [City]
+        
         switch segmentControl.selectedSegmentIndex {
         case 0:
             //dump(cityInfo.city)
-            return cityInfo.city
+            filteredCityList =  cityInfo.city
         case 1: // 국내
             //dump(cityInfo.city.filter { $0.domestic_travel == true })
-            return cityInfo.city.filter { $0.domestic_travel == true }
+            filteredCityList =  cityInfo.city.filter { $0.domestic_travel == true }
         case 2: // 해외
             //dump(cityInfo.city.filter { $0.domestic_travel == false })
-            return cityInfo.city.filter { $0.domestic_travel == false }
+            filteredCityList = cityInfo.city.filter { $0.domestic_travel == false }
         default:
-            return cityInfo.city
+            filteredCityList =  cityInfo.city
         }
+        
+        guard let searchText = textField.text,
+              !searchText.isEmpty else {return filteredCityList}
+        
+        
+        // 모두, 국내, 해외에서 필터링 후 -> 이거에 맞는 검색
+        let filterResult = filteredCityList.filter {
+            $0.city_name.lowercased().contains(searchText) ||
+            $0.city_english_name.lowercased().contains(searchText) ||
+            $0.city_explain.lowercased().contains(searchText)
+        }
+        
+        
+        return filterResult
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -70,6 +88,14 @@ class CityTableViewController: UITableViewController {
         default:
             print("에러")
         }
+        tableView.reloadData()
+    }
+    
+    
+    @IBAction func edtingDidEnd(_ sender: UITextField) {
+        tableView.reloadData()
+    }
+    @IBAction func edtingChanged(_ sender: UITextField) {
         tableView.reloadData()
     }
 }
