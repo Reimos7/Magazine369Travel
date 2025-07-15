31e19d7c4a0c93ed1840ccbc9f590aa8da86b8aa
Reimos7 <tjrvud9@gmail.com>
2025-07-16 02:56:42 +0900

    feat: TravelInfo - 광고 셀 클릭 시 값 전달 구현

--- Magazine369Travel/TravelInfo/TravelAdViewController.swift
+++ Magazine369Travel/TravelInfo/TravelAdViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class TravelAdViewController: UIViewController {
+    
+    @IBOutlet var mainLabel: UILabel!
+    var mainLabelText: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,6 +20,11 @@ class TravelAdViewController: UIViewController {
         // 네비게이션 좌측 상단 x 버튼 설정
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "xmark"), style: .plain, target: self, action: #selector(tappedCloseButton))
         navigationItem.leftBarButtonItem?.tintColor = .black
+        
+        mainLabel.text = mainLabelText
+        mainLabel.textAlignment = .center
+        mainLabel.font = .boldSystemFont(ofSize: 16)
+        mainLabel.numberOfLines = 0
     }
     
     // MARK: - dismiss
--- Magazine369Travel/TravelInfo/TravelTableViewController.swift
+++ Magazine369Travel/TravelInfo/TravelTableViewController.swift
@@ -65,13 +65,14 @@ class TravelTableViewController: UITableViewController {
     
     // MARK: - 광고 셀 클릭시 toastMessage 하단에 보여주기
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let travelInfoCell = travelInfo.travel[indexPath.row]
         if let ad = travelInfo.travel[indexPath.row].ad, ad {
             // toast 메시지
             // duration 몇 초 동안 보여줄지
             // position 위치, top이나 center는 키보드 때매 올라가니까 그럴때 사용하긴 함
             // 뷰 기준으로 나오는거니까 다르게 사용하면 제대로 적용 안될 가능성이 큼
             self.navigationController?.view.makeToast("광고 셀입니다", duration: 2.0, position: .bottom)
-            presentFeature()
+            presentFeature(title: travelInfoCell.title)
             
         } else {
             pushFeature()
@@ -99,11 +100,14 @@ class TravelTableViewController: UITableViewController {
     
     
     // MARK: - TravelAdViewController로 presnt를 통한 화면 이동 구현
-    private func presentFeature() {
+    private func presentFeature(title: String) {
         print(#function)
         let sb = UIStoryboard(name: "Main", bundle: nil)
         let vc = sb.instantiateViewController(withIdentifier: "TravelAdViewController") as! TravelAdViewController
         let nav = UINavigationController(rootViewController: vc)
+        // 데이터 전달
+        vc.mainLabelText = title
+        
         // 스타일 적용
         nav.modalPresentationStyle = .fullScreen
         //vc.modalTransitionStyle = .coverVertical
