dd97557f2de9d29f61f6d3dd85c3180e872ecb2d
Reimos7 <tjrvud9@gmail.com>
2025-07-15 16:26:15 +0900

    feat: travelVC - pop 기능 구현 및 이미지, 레이블, 버튼 UI 세팅

--- Magazine369Travel/TravelInfo/TravelViewController.swift
+++ Magazine369Travel/TravelInfo/TravelViewController.swift
@@ -8,13 +8,54 @@
 import UIKit
 
 class TravelViewController: UIViewController {
-
+    
+    @IBOutlet var travelImageView: UIImageView!
+    
+    @IBOutlet var traveltitleLabel: UILabel!
+    @IBOutlet var travelDescriptionLabel: UILabel!
+    
+    @IBOutlet var popButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationItem.title = "관광지 화면"
+        
+        setupLabel()
+        setupButton()
+    }
+    
+    func setupImage() {
+        travelImageView.contentMode = .scaleAspectFill
+        travelImageView.clipsToBounds = true
+        travelImageView.layer.cornerRadius = 14
+    }
+    
+    func setupLabel() {
+        traveltitleLabel.textAlignment = .center
+        traveltitleLabel.font = .boldSystemFont(ofSize: 20)
+        
+        // AutomaticDimension을 위해서 세팅
+        travelDescriptionLabel.numberOfLines = 0
+        travelDescriptionLabel.textAlignment = .center
+        travelDescriptionLabel.font = .boldSystemFont(ofSize: 18)
+    }
+    
+    func setupButton() {
+        popButton.setTitle("다른 광광지 보러가기", for: .normal)
+        popButton.setTitleColor(.white, for: .normal)
+        
+        popButton.clipsToBounds = true
+        popButton.layer.cornerRadius = 14
+        
+        popButton.backgroundColor =  UIColor(red: 135/255.0, green: 186/255.0, blue: 234/255.0, alpha: 1.0)
         
     }
     
-
+    // MARK: - pop으로 뒤로 가기
+    @IBAction func tappedPopButton(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+        
+    }
+    
    
 }
