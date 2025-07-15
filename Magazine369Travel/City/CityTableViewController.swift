1c407a85b2bf9a3a859abf0a45dd04d38b5eff1e
Reimos7 <tjrvud9@gmail.com>
2025-07-16 00:12:20 +0900

    feat: 세그먼트 선택에 따라 해당 도시를 테이블뷰에 출력

--- Magazine369Travel/City/CityTableViewController.swift
+++ Magazine369Travel/City/CityTableViewController.swift
@@ -11,7 +11,9 @@ import Kingfisher
 class CityTableViewController: UITableViewController {
     
     let cityInfo = CityInfo()
-
+    
+    @IBOutlet var segmentControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "인기 도시"
@@ -22,17 +24,52 @@ class CityTableViewController: UITableViewController {
         tableView.rowHeight = 200
     }
     
+    
+    // MARK: - 0 , 1 , 2 조건에 따라 
+    func filteredCity() -> [City] {
+        switch segmentControl.selectedSegmentIndex {
+        case 0:
+            //dump(cityInfo.city)
+            return cityInfo.city
+        case 1: // 국내
+            //dump(cityInfo.city.filter { $0.domestic_travel == true })
+            return cityInfo.city.filter { $0.domestic_travel == true }
+        case 2: // 해외
+            //dump(cityInfo.city.filter { $0.domestic_travel == false })
+            return cityInfo.city.filter { $0.domestic_travel == false }
+        default:
+            return cityInfo.city
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cityInfo.city.count
+        return filteredCity().count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cityInfoCell = cityInfo.city[indexPath.row]
+        //let cityInfoCell = cityInfo.city[indexPath.row]
+        let cityInfoCell = filteredCity()[indexPath.row]
         
         let cell = tableView.dequeueReusableCell(withIdentifier: CityTableViewCell.identifier, for: indexPath) as! CityTableViewCell
         
         cell.configure(cityInfoCell: cityInfoCell)
         
         return cell
+    }
+    
+    
+    // MARK: - 세그먼트 컨트롤러 클릭 시 테이블뷰 reload
+    @IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            print("모두")
+        case 1:
+            print("국내")
+        case 2:
+            print("해외")
+        default:
+            print("에러")
+        }
+        tableView.reloadData()
     }
 }
